51194dca5d4b84e5dd05757f9c3480d6da630813
Júlio Luwungu <julioluwungu@gmail.com>
2025-09-12 00:48:54 +0100

    estudando JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 51
+console.log(`Você está dirigindo a ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você foi MULTADO por excesso de velocidade.')
+}
+console.log('Dirija sempre com cinto de segurança!')
