74b7a7ceddb65445ed533e4e3e9f181692133b49
Júlio Luwungu <julioluwungu@gmail.com>
2025-10-04 00:08:58 +0100

    exercitando JS

---
+++ aula16ex/modelo/scripts.js
@@ -0,0 +1,50 @@
+let vetor = []
+let div = document.createElement('div')
+let formulario = document.getElementsByTagName('form')[0]
+let sel = document.getElementById('seldados')
+formulario.appendChild(div)
+
+function clicou() {
+    div.innerHTML = ''
+    let num = Number(document.getElementById('numero').value)
+    let opcao = document.createElement('option')
+    if (vetor.indexOf(num) != -1 || num > 100 || num < 1) {
+        window.alert('Valor inválido ou já adicionado na lista!')
+    } else {
+        vetor.push(num)
+        opcao.innerHTML = `Valor ${num} adicionado`
+        sel.appendChild(opcao)
+    }
+}
+
+function resultado() {
+    if (vetor.length != 0) {
+        let maior = vetor[0]
+        let menor = vetor[0]
+        let soma = 0
+        for (let pos in vetor) {
+            soma += vetor[pos]
+            if (vetor[pos] > maior) {
+                maior = vetor[pos]
+            }
+            if (vetor[pos] < menor) {
+                menor = vetor[pos]
+            }
+        }
+        let media = soma / vetor.length
+
+        div.innerHTML = `Ao todo temos ${vetor.length} números cadastrados.<br>`
+        div.innerHTML += `O maior valor informado foi ${maior}.<br>`
+        div.innerHTML += `O menor valor informado foi ${menor}.<br>`
+        div.innerHTML += `Somando todos os valores, temos ${soma}.<br>`
+        div.innerHTML += `A média dos valores digitados é ${media.toFixed(2)}.<br>`
+    } else {
+        window.alert('Adicione valores antes de finalizar!')
+    }
+}
+
+function limpar() {
+    div.innerHTML = ''
+    sel.innerHTML = ''
+    vetor = []
+}
